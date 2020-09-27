5c9572d7e39664e8d763ab597da218cc0dbc3b7e
Kakkattil, Jenson Jos <jenson.kakkattil@ge.com>
2020-09-27 16:49:03 -0400

    initial commit

---
+++ src/main/java/net/codejava/ClientA.java
@@ -0,0 +1,22 @@
+package net.codejava;
+
+public class ClientA implements Client {
+
+    Service service;
+
+    public ClientA(Service service) {
+
+        this.service = service;
+    }
+
+    public void doSomething() {
+
+        String info = service.getInfo();
+        System.out.println(info);
+    }
+
+    public void setService(Service service) {
+
+        this.service = service;
+    }
+}
---
+++ src/main/java/net/codejava/ServiceB.java
@@ -0,0 +1,8 @@
+package net.codejava;
+
+public class ServiceB implements Service {
+    public String getInfo() {
+
+        return "ServiceB's Info";
+    }
+}
